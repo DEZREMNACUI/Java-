b0d2ceee0c63f37caa51822ee78ce12a646a5c06
DEZREMNACUI <993261554@qq.com>
2023-11-20 13:25:30 +0800

    更新了搜索，分页功能

--- src/main/java/DatabaseReader.java
+++ src/main/java/DatabaseReader.java
@@ -9,20 +9,27 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-@WebServlet( urlPatterns = "/DatabaseReader")
+@WebServlet(urlPatterns = "/DatabaseReader")
 public class DatabaseReader extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setHeader("Access-Control-Allow-Origin", "*"); // 允许任何来源访问
-        resp.setHeader("Access-Control-Allow-Methods", "GET"); // 允许 GET 请求
-        resp.setHeader("Access-Control-Allow-Methods", "POST");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "GET");
         resp.setHeader("Content-Type", "application/json");
+
         String jdbcUrl = "jdbc:mysql://10.69.219.123:3306/db2";
         String username = "1";
         String password = "13550581080";
 
-        List<Dataitem> dataList = Dataitem.fetchDataFromDatabase(jdbcUrl, username, password);
+        String searchType = req.getParameter("searchType");
+        String filter = req.getParameter("filter");
+        int page = Integer.parseInt(req.getParameter("page")); // 获取传递的页码参数
+
+        int pageSize = 3; // 每页显示的数据量
+        int offset = (page - 1) * pageSize; // 计算偏移量
+
+        List<Dataitem> dataList = Dataitem.fetchDataFromDatabase(jdbcUrl, username, password, searchType, filter, offset, pageSize);
 
         String jsonString = JSON.toJSONString(dataList);
         resp.setContentType("application/json");
--- src/main/java/Dataitem.java
+++ src/main/java/Dataitem.java
@@ -34,7 +34,8 @@ public class Dataitem {
                 '}';
     }
 
-    public static List<Dataitem> fetchDataFromDatabase(String jdbcUrl, String username, String password) {
+    public static List<Dataitem> fetchDataFromDatabase(String jdbcUrl, String username, String password,
+                                                       String searchType, String filter, int offset, int pageSize) {
         List<Dataitem> dataList = new ArrayList<>();
 
         try {
@@ -43,20 +44,36 @@ public class Dataitem {
 
             // 建立数据库连接
             try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
-                // 执行查询
-                String sql = "SELECT 书名, 作者, 国籍 FROM bookstore";
-                try (PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                     ResultSet resultSet = preparedStatement.executeQuery()) {
+                // 构建查询语句
+                String sql = "";
+                switch (searchType) {
+                    case "book":
+                        sql = "SELECT 书名, 作者, 国籍 FROM bookstore WHERE 书名 LIKE ? LIMIT ?, ?";
+                        break;
+                    case "author":
+                        sql = "SELECT 书名, 作者, 国籍 FROM bookstore WHERE 作者 LIKE ? LIMIT ?, ?";
+                        break;
+                    // 添加其他搜索类型的逻辑
+                }
 
-                    // 处理查询结果
-                    while (resultSet.next()) {
-                        String book = resultSet.getString("书名");
-                        String author = resultSet.getString("作者");
-                        String nation = resultSet.getString("国籍");
+                try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                    // 设置参数
+                    preparedStatement.setString(1, "%" + filter + "%");
+                    preparedStatement.setInt(2, offset);
+                    preparedStatement.setInt(3, pageSize);
 
-                        // 创建数据项对象并添加到列表中
-                        Dataitem dataItem = new Dataitem(book, author, nation);
-                        dataList.add(dataItem);
+                    // 执行查询
+                    try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                        // 处理查询结果
+                        while (resultSet.next()) {
+                            String book = resultSet.getString("书名");
+                            String author = resultSet.getString("作者");
+                            String nation = resultSet.getString("国籍");
+
+                            // 创建数据项对象并添加到列表中
+                            Dataitem dataItem = new Dataitem(book, author, nation);
+                            dataList.add(dataItem);
+                        }
                     }
                 }
             }
@@ -65,6 +82,7 @@ public class Dataitem {
         }
         return dataList;
     }
+
 
     public static void updateDataInDatabase(String jdbcUrl, String username, String password,
                                             String originalBook, String originalAuthor, String originalNation,
@@ -160,5 +178,46 @@ public class Dataitem {
             e.printStackTrace();
         }
     }
+//    public static List<Dataitem> fetchDataWithSearch(String jdbcUrl, String username, String password, String searchType, String filter) {
+//        List<Dataitem> dataList = new ArrayList<>();
+//
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//
+//            try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+//                String sql = "";
+//
+//                switch (searchType) {
+//                    case "book":
+//                        sql = "SELECT 书名, 作者, 国籍 FROM bookstore WHERE 书名 LIKE ?";
+//                        break;
+//                    case "author":
+//                        sql = "SELECT 书名, 作者, 国籍 FROM bookstore WHERE 作者 LIKE ?";
+//                        break;
+//                    // 添加其他搜索类型的逻辑
+//                }
+//
+//                try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+//                    preparedStatement.setString(1, "%" + filter + "%");
+//
+//                    try (ResultSet resultSet = preparedStatement.executeQuery()) {
+//                        while (resultSet.next()) {
+//                            String book = resultSet.getString("书名");
+//                            String author = resultSet.getString("作者");
+//                            String nation = resultSet.getString("国籍");
+//
+//                            Dataitem dataItem = new Dataitem(book, author, nation);
+//                            dataList.add(dataItem);
+//                        }
+//                    }
+//                }
+//            }
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return dataList;
+//    }
+
 
 }
